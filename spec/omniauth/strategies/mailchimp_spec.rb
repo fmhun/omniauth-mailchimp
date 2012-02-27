a6b42cc827c1ae91d4838d60473b27eb43bc6a2a
Florian Mhun <florian.mhun@gmail.com>
2012-02-28 00:01:23 +0100

    added some tests

--- spec/omniauth/strategies/mailchimp_spec.rb
+++ spec/omniauth/strategies/mailchimp_spec.rb
@@ -15,9 +15,23 @@ describe OmniAuth::Strategies::Mailchimp do
 
   it_should_behave_like 'an oauth2 strategy'
 
-  describe '#client' do
-    it 'has correct MailChimp site' do
-      subject.client.site.should eq('https://login.mailchimp.com')
+	describe '#client' do
+    it 'has correct Mailchimp api site' do
+      subject.options.client_options.site.should eq('https://login.mailchimp.com')
+    end
+
+    it 'has correct access token path' do
+      subject.options.client_options.token_url.should eq('/oauth2/token')
+    end
+
+    it 'has correct authorize url' do
+      subject.options.client_options.authorize_url.should eq('/oauth2/authorize')
+    end
+  end
+
+	describe '#callback_path' do
+    it 'should have the correct callback path' do
+      subject.callback_path.should eq('/auth/mailchimp/callback')
     end
   end
 end
